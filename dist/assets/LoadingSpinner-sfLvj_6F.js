98209ebbd22b722334110a4902608c241aadcb86
Desos_Wajos <55079306+Desire1990@users.noreply.github.com>
2026-08-08 19:18:49 +0200

    Add files via upload
    dist updated

---
+++ dist/assets/LoadingSpinner-sfLvj_6F.js
@@ -0,0 +1,1 @@
+import{_ as n,o as e,c}from"./index-6Li9johj.js";const o={},s={class:"spinner"};function r(t,_){return e(),c("div",s,"Loading...")}const i=n(o,[["render",r]]);export{i as L};
---
+++ dist/assets/categories-DdmBqhhP.js
@@ -0,0 +1,1 @@
+import{k as e}from"./index-6Li9johj.js";const o=()=>e.get("/categories/");export{o as g};
